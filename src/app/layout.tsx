5c1052f1e313e4800979f02fb33ff089152176a8
Rodrigodev1998 <rodrigofersouza5@gmail.com>
2026-01-07 11:47:28 -0300

    ajuste nas estruturas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { AuthProvider } from "@/context/AuthContext";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "FeirApp",
@@ -24,7 +20,6 @@ export default function RootLayout({
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          geistSans.className
         )}
       >
         <AuthProvider>
